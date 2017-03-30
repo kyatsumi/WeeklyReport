d384741607cd957ac1c95498c0a358f60fd4394e
SCS036 <SCS036@PC-SCS21.netscs.local>
2017-03-30 09:35:47 +0900

    定数クラスの作成

---
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/util/LineBotConstant.java
@@ -0,0 +1,29 @@
+package jp.co.netscs.weeklyreport.linesystem.commons.util;
+
+/**
+ * LineBot用定数クラス
+ * @author SCS036
+ *
+ */
+public class LineBotConstant {
+	
+	/**
+	 *　機能名：ユーザ登録
+	 */
+	public static final String SCTION_REGIST = "ユーザ登録";
+	
+	/**
+	 * 機能名：週報登録
+	 */
+	public static final String SCTION_REPORT = "週報登録";
+
+	/**
+	 * 機能名：週報確認
+	 */
+	public static final String SCTION_REPORTVIEW = "週報確認";
+	
+	/**
+	 * 機能名：全体の週報確認
+	 */
+	public static final String SCTION_TEAMREPORTVIEW = "全体の週報確認";
+}
