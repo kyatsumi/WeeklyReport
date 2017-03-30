9a1a967006b36f2763c2455fe2efb018ced788e0
SCS036 <SCS036@PC-SCS21.netscs.local>
2017-03-30 16:23:10 +0900

    自動呼出し機能の実装

--- src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/WeeklyReportMessageServiceImpl.java
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/WeeklyReportMessageServiceImpl.java
@@ -1,14 +1,18 @@
 package jp.co.netscs.weeklyreport.linesystem.commons;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TextMessage;
 
+import jp.co.netscs.weeklyreport.linesystem.commons.annot.Section;
+import jp.co.netscs.weeklyreport.linesystem.commons.daos.LineSceneDao;
 import jp.co.netscs.weeklyreport.linesystem.commons.dtos.LinePostInfoDto;
 import jp.co.netscs.weeklyreport.linesystem.commons.dtos.LineSectionDto;
 
@@ -20,16 +24,47 @@ import jp.co.netscs.weeklyreport.linesystem.commons.dtos.LineSectionDto;
 @Service
 public class WeeklyReportMessageServiceImpl implements WeeklyReportMessageService {
 
+	@Autowired
+	private LineSceneDao lineSeceneDao;
+	
 	/**
 	 * 
 	 */
 	@Transactional
 	@Override
 	public List<Message> execute(LinePostInfoDto lineInfo, LineSectionDto section) {
+		List<Field> fieldList = Arrays.asList(this.getClass().getFields());
+		List<Field> targetField = fieldList.stream().filter(field -> field.isAnnotationPresent(Section.class))
+			.filter(field -> ((Section)field.getAnnotation(Section.class)).name().equals(section.getSection()))
+			.collect(Collectors.toList());
+		if (targetField.isEmpty()) {
+			throw new RuntimeException("対象のセクションがありません。");
+		}
 		
+		if (targetField.size() > 1) {
+			throw new RuntimeException("対象のセクションが重複しています。");
+		}
 		
-		return null;
+		Field target = targetField.get(0);
+		Object targetSection = null;
+		try {
+			targetSection = target.get(this);
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		List<Message> result = null;
+		if (targetSection instanceof AbstractSectionService) {
+			AbstractSectionService sectionService = (AbstractSectionService) targetSection;
+			result = sectionService.execute(section.getScene(), lineInfo);
+		}
+		
+		return result;
 	}
-	
 
 }
