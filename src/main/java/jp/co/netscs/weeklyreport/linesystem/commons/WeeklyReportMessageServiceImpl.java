58e10f72ef6e5bbfe3fb217e6cb09f060b96441a
SCS036 <SCS036@PC-SCS21.netscs.local>
2017-03-30 13:13:37 +0900

    セクション取得確認

--- src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/WeeklyReportMessageServiceImpl.java
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/WeeklyReportMessageServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
@@ -11,13 +12,22 @@ import com.linecorp.bot.model.message.TextMessage;
 import jp.co.netscs.weeklyreport.linesystem.commons.dtos.LinePostInfoDto;
 import jp.co.netscs.weeklyreport.linesystem.commons.dtos.LineSectionDto;
 
+/**
+ * 
+ * @author SCS036
+ *
+ */
 @Service
 public class WeeklyReportMessageServiceImpl implements WeeklyReportMessageService {
 
+	/**
+	 * 
+	 */
+	@Transactional
 	@Override
 	public List<Message> execute(LinePostInfoDto lineInfo, LineSectionDto section) {
 		return Arrays.asList(
-				new TextMessage(lineInfo.getText())
+				new TextMessage(section.toString())
 				);
 	}
 
