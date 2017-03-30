90befb39c280a1ce3069e85c8ea90cae2fe819c4
SCS036 <SCS036@PC-SCS21.netscs.local>
2017-03-30 13:12:12 +0900

    セクション取得処理を実装

--- src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/WeeklyReportSectionServiceImpl.java
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/WeeklyReportSectionServiceImpl.java
@@ -1,13 +1,24 @@
 package jp.co.netscs.weeklyreport.linesystem.commons;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jp.co.netscs.weeklyreport.linesystem.commons.daos.LineSceneDao;
+import jp.co.netscs.weeklyreport.linesystem.commons.daos.UserDao;
 import jp.co.netscs.weeklyreport.linesystem.commons.dtos.LinePostInfoDto;
 import jp.co.netscs.weeklyreport.linesystem.commons.dtos.LineSectionDto;
+import jp.co.netscs.weeklyreport.linesystem.commons.dtos.LineSectionDto.LineSectionDtoBuilder;
+import jp.co.netscs.weeklyreport.linesystem.commons.entitis.LineSceneEntity;
 import jp.co.netscs.weeklyreport.linesystem.commons.util.LineBotConstant;
 
 @Service
 public class WeeklyReportSectionServiceImpl implements WeeklyReportSectionService {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@Autowired
+	private LineSceneDao lineSceneDao;
 
 	/** 
 	 * TODO 仮実装の廃止
@@ -15,9 +26,15 @@ public class WeeklyReportSectionServiceImpl implements WeeklyReportSectionServic
 	 */
 	@Override
 	public LineSectionDto fetchUserSection(LinePostInfoDto lineInfo, boolean isPostBack) {
-		return LineSectionDto.builder()
-				.scene(LineBotConstant.SCTION_REGIST)
-				.section(LineBotConstant.REGIST_SCENE_START).build();
+		LineSectionDtoBuilder result = LineSectionDto.builder();
+		
+		if (userDao.exists(lineInfo.getUserId())) {
+			result.section(LineBotConstant.SCTION_REGIST).scene(LineBotConstant.REGIST_SCENE_START);
+		}
+		
+		LineSceneEntity sectionInfo =  lineSceneDao.getOne(lineInfo.getUserId());
+		result.scene(sectionInfo.getScene()).section(sectionInfo.getSection());
+		return result.build();
 	}
 
 }
