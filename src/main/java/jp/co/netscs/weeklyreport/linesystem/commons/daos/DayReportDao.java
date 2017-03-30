78d13c8e452a30c44ec0362ddf13446fa47bbf8a
SCS036 <SCS036@PC-SCS21.netscs.local>
2017-03-30 11:28:19 +0900

    データベース層に@Repositoryを追加

--- src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/daos/DayReportDao.java
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/daos/DayReportDao.java
@@ -1,9 +1,11 @@
 package jp.co.netscs.weeklyreport.linesystem.commons.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import jp.co.netscs.weeklyreport.linesystem.commons.entitis.DayReportEntity;
 
+@Repository
 public interface DayReportDao extends JpaRepository<DayReportEntity, Long> {
 
 }
--- src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/daos/UserDao.java
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/daos/UserDao.java
@@ -1,9 +1,11 @@
 package jp.co.netscs.weeklyreport.linesystem.commons.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import jp.co.netscs.weeklyreport.linesystem.commons.entitis.UserEntity;
 
+@Repository
 public interface UserDao extends JpaRepository<UserEntity, Long> {
 
 }
