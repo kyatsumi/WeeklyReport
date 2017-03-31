21405c927393580595990ef7c4b740f401444007
SCS036 <SCS036@PC-SCS21.netscs.local>
2017-03-31 11:19:04 +0900

    トランザクションの追加

--- src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/daos/LineSceneDao.java
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/daos/LineSceneDao.java
@@ -2,9 +2,11 @@ package jp.co.netscs.weeklyreport.linesystem.commons.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.netscs.weeklyreport.linesystem.commons.entitis.LineSceneEntity;
 
+@Transactional
 @Repository
 public interface LineSceneDao extends JpaRepository<LineSceneEntity, String>{
 	
