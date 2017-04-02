27900f56633720e4f5226154bff99f53c12474ae
katsumi <k.m.penkoka@gmail.com>
2017-04-03 00:03:56 +0900

    ユーザ新規登録画面を変更

--- src/main/java/jp/co/netscs/weeklyreport/linesystem/regist/RegistServiceImpl.java
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/regist/RegistServiceImpl.java
@@ -37,7 +37,7 @@ public class RegistServiceImpl extends RegistService {
 	public List<Message> start(LinePostInfoDto lineInfo) {
 		UserEntity userInfo = UserEntity.builder().admin(false).group(null).lineId(lineInfo.getUserId()).name(null).build();
 		userDao.save(userInfo);
-		Message message = LineMessageUtils.confirm("ユーザ登録", "管理者権限が必要ですか？", "はい", "いいえ");
+		Message message = LineMessageUtils.confirm("ユーザ登録", "新規登録を行います。\n管理者権限が必要ですか？", "はい", "いいえ");
 		return Arrays.asList(message);
 	}
 
