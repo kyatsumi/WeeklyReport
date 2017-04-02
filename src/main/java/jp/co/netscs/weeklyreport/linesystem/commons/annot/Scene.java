b30b180211d2d2d8d1fd15dfc83bcebc39433cce
katsumi <k.m.penkoka@gmail.com>
2017-04-02 16:00:03 +0900

    アノテーションを継承しても有効になるように変更

--- src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/annot/Scene.java
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/annot/Scene.java
@@ -1,6 +1,7 @@
 package jp.co.netscs.weeklyreport.linesystem.commons.annot;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -22,7 +23,7 @@ import jp.co.netscs.weeklyreport.linesystem.commons.util.LineBotConstant;
  * @author SCS036
  *
  */
-
+@Inherited
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Scene {
--- src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/annot/Section.java
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/commons/annot/Section.java
@@ -1,6 +1,7 @@
 package jp.co.netscs.weeklyreport.linesystem.commons.annot;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -16,7 +17,7 @@ import java.lang.annotation.Target;
  * @author SCS036
  *
  */
-
+@Inherited
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Section {
