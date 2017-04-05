5c846c4d3627b0787721576bb937836c5c036f41
SCS036 <SCS036@PC-SCS21.netscs.local>
2017-04-05 10:41:18 +0900

    アノテーションの追加

---
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/common/annotation/SceneAfter.java
@@ -0,0 +1,20 @@
+package jp.co.netscs.weeklyreport.linesystem.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * シーン処理実行後に入力があった場合の情報を受け取る
+ * @author SCS036
+ *
+ */
+
+@Inherited
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SceneAfter {
+
+}
