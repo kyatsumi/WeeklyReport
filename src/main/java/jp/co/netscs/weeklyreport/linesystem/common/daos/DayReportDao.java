c9560bdfdd993157908635de3cd7935400ce1758
katsumi <k.m.penkoka@gmail.com>
2017-04-23 02:32:43 +0900

    代替え変数を使用

--- src/main/java/jp/co/netscs/weeklyreport/linesystem/common/daos/DayReportDao.java
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/common/daos/DayReportDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +16,7 @@ import jp.co.netscs.weeklyreport.linesystem.common.entitis.ReportEntity;
 @Repository
 public interface DayReportDao extends JpaRepository<DayReportEntity, ReportEntity> {
 	
-	@Query("select report from DayReport report where report.date between ?1 and ?2 and report.lineid = ?3 order by report.date")
-	List<DayReportEntity> findByLineidAndDateBetweenOrderByDate(Date startDate, Date endDate, String lineid);
+	@Query("select report from DayReport report where report.date between :startDate and :endDate and report.lineid = :lineid order by report.date")
+	List<DayReportEntity> findByLineidAndDateBetweenOrderByDate(@Param("startDate") Date startDate, @Param("endDate")Date endDate, @Param("lineid")String lineid);
 
 }
