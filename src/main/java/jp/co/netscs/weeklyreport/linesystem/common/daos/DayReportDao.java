468f80cae2d644118b0f2642a6ee79b676c511ac
katsumi <k.m.penkoka@gmail.com>
2017-04-23 02:10:39 +0900

    範囲指定で日報を取得するメソッドの追加

--- src/main/java/jp/co/netscs/weeklyreport/linesystem/common/daos/DayReportDao.java
+++ src/main/java/jp/co/netscs/weeklyreport/linesystem/common/daos/DayReportDao.java
@@ -1,6 +1,10 @@
 package jp.co.netscs.weeklyreport.linesystem.common.daos;
 
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,5 +14,8 @@ import jp.co.netscs.weeklyreport.linesystem.common.entitis.ReportEntity;
 @Transactional
 @Repository
 public interface DayReportDao extends JpaRepository<DayReportEntity, ReportEntity> {
+	
+	@Query("select report from DayReportEntity report where report.date between ?1 and ?2 and report.lineid = ?3 order by report.date")
+	List<DayReportEntity> findByLineidAndDateBetweenOrderByDate(Date startDate, Date endDate, String lineid);
 
 }
